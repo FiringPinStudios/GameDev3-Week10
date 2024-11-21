f5909f7c8dd9bd5fc57ea9efe1f6eaafd628eb09
FiringPinStudios <130931434+FiringPinStudios@users.noreply.github.com>
2024-11-21 12:18:47 +0000

    ObjectPoolAdded

--- Game Dev 3 - Week 10/Assets/_Scripts/Managers/ObjectPooling/PoolData.cs
+++ Game Dev 3 - Week 10/Assets/_Scripts/Managers/ObjectPooling/PoolData.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace GameDevWithMarco.DesignPattern
@@ -6,17 +5,9 @@ namespace GameDevWithMarco.DesignPattern
     [CreateAssetMenu(fileName = "Pool", menuName = "Scriptable Objects/Pool")]
     public class PoolData : ScriptableObject
     {
-        //To store the pool
-        public List<GameObject> pooledObjectContainer;
         //To set the size of the pool
         public int poolAmount = 40;
         //To store the prefab to use in the pool
         public GameObject poolItem;
-
-        //To clear the pool
-        public void ResetThePool()
-        {
-            pooledObjectContainer.Clear();
-        }
     }
 }
